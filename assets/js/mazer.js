8bb30478766190f11dd1c434f1ac3f89722aea7f
iamjhon <iamjhon801@gmail.com>
2024-12-16 22:35:48 +0600

    first commit

---
+++ assets/js/mazer.js
@@ -0,0 +1,7 @@
+// Load Bootstrap
+import bootstrapBundle from "bootstrap/dist/js/bootstrap.bundle"
+window.bootstrap = bootstrapBundle
+
+// We could import PerfectScrollbar directly in the sidebar module
+import "./../static/js/components/sidebar"
+
